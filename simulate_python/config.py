9f60255eb8327c1954a2a972d1ca4584b0c8d148
yangning wu <rd_wyn@unitree.com>
2024-09-03 21:09:29 +0800

    add go2w

--- simulate_python/config.py
+++ simulate_python/config.py
@@ -1,5 +1,5 @@
-ROBOT = "go2" # Robot name, "go2", "b2", "b2w", "h1" 
-ROBOT_SCENE = "../unitree_robots/" + ROBOT + "/scene.xml" # Robot scene
+ROBOT = "go2" # Robot name, "go2", "b2", "b2w", "h1", "go2w" 
+ROBOT_SCENE = "../unitree_robots/" + ROBOT + "/scene_terrain.xml" # Robot scene
 DOMAIN_ID = 1 # Domain id
 INTERFACE = "lo" # Interface 
 
@@ -10,5 +10,5 @@ JOYSTICK_DEVICE = 0 # Joystick number
 PRINT_SCENE_INFORMATION = True # Print link, joint and sensors information of robot
 ENABLE_ELASTIC_BAND = False # Virtual spring band, used for lifting h1
 
-SIMULATE_DT = 0.003  # Need to be larger than the runtime of viewer.sync()
+SIMULATE_DT = 0.005  # Need to be larger than the runtime of viewer.sync()
 VIEWER_DT = 0.02  # 50 fps for viewer
